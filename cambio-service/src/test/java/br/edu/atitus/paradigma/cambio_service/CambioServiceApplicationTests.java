2b5815c535fa30287d3c391f75e322e759baa4f1
Nycolas Musskopf Fachi <135671583+nycolasfachi1701@users.noreply.github.com>
2024-10-31 18:27:27 -0300

    Adicionando demais informações

--- cambio-service/src/test/java/br/edu/atitus/paradigma/cambio_service/CambioServiceApplicationTests.java
+++ cambio-service/src/test/java/br/edu/atitus/paradigma/cambio_service/CambioServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package br.edu.atitus.paradigma.eureka_service;
+package br.edu.atitus.paradigma.cambio_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServiceApplicationTests {
+class CambioServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
